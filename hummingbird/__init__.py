9a2e9adb05b0fa534484879870103df8f881ca31
Ubuntu <hummingbird@supun-p100-large.wv4ydpj3uqtevgy1qawe5v0n4b.xx.internal.cloudapp.net>
2020-05-02 01:40:19 +0000

    add tighter API refactoring of supported file fix to examples and notebook to mirror the new tight API

--- hummingbird/__init__.py
+++ hummingbird/__init__.py
@@ -17,31 +17,39 @@ __producer_version__ = __version__
 __domain__ = "microsoft.gsl"
 __model_version__ = 0
 
+
 # Register constants used for Hummingbird extra configs.
-from . import supported_configurations as hummingbird_constants
+from . import supported as hummingbird_constants
 from ._utils import _Constants
 
 # Add constants in scope.
 constants = _Constants(hummingbird_constants)
 
+
 # Add the converters in the Hummingbird scope.
-from .convert import to_pytorch_sklearn  # noqa: F401
-from .convert import to_pytorch_lightgbm  # noqa: F401
-from .convert import to_pytorch_xgboost  # noqa: F401
+from .convert import _to_pytorch_sklearn  # noqa: F401
+from .convert import _to_pytorch_lightgbm  # noqa: F401
+from .convert import _to_pytorch_xgboost  # noqa: F401
 
-from sklearn.ensemble import (
-    RandomForestClassifier,
-    RandomForestRegressor,
-    GradientBoostingClassifier,
-    ExtraTreesClassifier,
-)  # noqa: F401
-from sklearn.tree import DecisionTreeClassifier  # noqa: F401
 
-RandomForestClassifier.to_pytorch = to_pytorch_sklearn
-RandomForestRegressor.to_pytorch = to_pytorch_sklearn
-GradientBoostingClassifier.to_pytorch = to_pytorch_sklearn
-ExtraTreesClassifier.to_pytorch = to_pytorch_sklearn
-DecisionTreeClassifier.to_pytorch = to_pytorch_sklearn
+# Set up the converter dispatcher. 
+from .supported import sklearn_operator_list  # noqa: F401
+from .supported import xgb_operator_list  # noqa: F401
+from .supported import lgbm_operator_list  # noqa: F401
+
+
+for operator in sklearn_operator_list:
+    if operator is not None:
+        operator.to = _to_pytorch_sklearn
+
+for operator in xgb_operator_list:
+    if operator is not None:
+        operator.to = _to_pytorch_xgboost
+
+for operator in lgbm_operator_list:
+    if operator is not None:
+        operator.to = _to_pytorch_lightgbm
+
 
 # Pdoc stuff.
 __pdoc__ = {}
