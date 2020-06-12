32cfc3b3be4fab4e5302acac0a1ddf6446ab1a5d
Karla Saur <kasaur@microsoft.com>
2020-06-12 08:47:26 -0700

    missing el in elif

--- setup.py
+++ setup.py
@@ -26,13 +26,13 @@ install_requires = ["numpy>=1.15", "onnxconverter-common>=1.6.0", "scikit-learn=
 if sys.platform == "darwin" or sys.platform == "linux":
     install_requires.append("torch==1.5.0")
 else:
-    if sys.version_info >= (3, 8, 0):
+    if sys.version_info >= (3, 8):
         install_requires.append("torch @ https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp38-cp38-win_amd64.whl")
-    elif sys.version_info >= (3, 7, 0):
+    elif sys.version_info >= (3, 7):
         install_requires.append("torch @ https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp37-cp37m-win_amd64.whl")
-    elif sys.version_info >= (3, 6, 0):
+    elif sys.version_info >= (3, 6):
         install_requires.append("torch @ https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp36-cp36m-win_amd64.whl")
-    if sys.version_info >= (3, 5, 0):
+    elif sys.version_info >= (3, 5):
         install_requires.append("torch @ https://download.pytorch.org/whl/cpu/torch-1.5.0%2Bcpu-cp35-cp35m-win_amd64.whl")
     else:
         raise Exception("Python version < 3.5 not supported.")
