1fc2b568505e561534c95c65ffedce509f15215b
Tuan Nguyen <anhtuan277@gmail.com>
2020-06-12 20:07:17 -0700

    Update test_sklearn_histgbdt_converters.py

--- tests/test_sklearn_histgbdt_converters.py
+++ tests/test_sklearn_histgbdt_converters.py
@@ -6,11 +6,7 @@ import warnings
 
 import numpy as np
 import torch
-
-from sklearn.ensemble._hist_gradient_boosting.gradient_boosting import (
-    HistGradientBoostingClassifier,
-    HistGradientBoostingRegressor,
-)
+from sklearn.ensemble._hist_gradient_boosting.gradient_boosting import HistGradientBoostingClassifier, HistGradientBoostingRegressor
 
 import hummingbird.ml
 from tree_utils import gbdt_implementation_map
@@ -27,22 +23,9 @@ class TestSklearnHistGradientBoostingClassifier(unittest.TestCase):
             y = np.random.randint(num_classes, size=100) + labels_shift
 
             model.fit(X, y)
-            torch_model = hummingbird.ml.convert(model, "torch", extra_config=extra_config)
-            self.assertTrue(torch_model is not None)
-            np.testing.assert_allclose(model.predict_proba(X), torch_model.predict_proba(X), rtol=1e-06, atol=1e-06)
-
-    def _run_GB_trees_regressor_converter(self, extra_config={}):
-        warnings.filterwarnings("ignore")
-        for max_depth in [2, 3, 8, 10, 12, None]:
-            model = HistGradientBoostingRegressor(max_iter=10, max_depth=max_depth)
-            np.random.seed(0)
-            X = np.random.rand(100, 200).astype(np.float32)
-            y = np.random.normal(size=100)
-
-            model.fit(X, y)
-            torch_model = hummingbird.ml.convert(model, "torch", extra_config=extra_config)
-            self.assertIsNotNone(torch_model)
-            np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-06, atol=1e-06)
+            pytorch_model = hummingbird.ml.convert(model, "pytorch", extra_config=extra_config)
+            self.assertTrue(pytorch_model is not None)
+            np.testing.assert_allclose(model.predict_proba(X), pytorch_model.predict_proba(X), rtol=1e-06, atol=1e-06)
 
 
     def _run_GB_trees_regressor_converter(self, extra_config=None):
@@ -104,23 +87,7 @@ class TestSklearnHistGradientBoostingClassifier(unittest.TestCase):
         X = np.array(X, dtype=np.float32)
         y = np.random.randint(3, size=100).astype(np.float32)  # y must be int, not float, should error
         model = HistGradientBoostingClassifier(max_iter=10).fit(X, y)
-        self.assertRaises(RuntimeError, hummingbird.ml.convert, model, "torch")
-
-    # Regressor
-    def test_GBDT_regressor_converter(self):
-        self._run_GB_trees_regressor_converter()
-
-    # Gemm regressor
-    def test_GBDT_gemm_regressor_converter(self):
-        self._run_GB_trees_regressor_converter(extra_config={"tree_implementation": "gemm"})
-
-    # Tree_trav regressor
-    def test_GBDT_tree_trav_regressor_converter(self):
-        self._run_GB_trees_regressor_converter(extra_config={"tree_implementation": "tree_trav"})
-
-    # Perf_tree_trav regressor
-    def test_GBDT_perf_tree_trav_regressor_converter(self):
-        self._run_GB_trees_regressor_converter(extra_config={"tree_implementation": "perf_tree_trav"})
+        self.assertRaises(RuntimeError, hummingbird.ml.convert, model, "pytorch")
 
 
     # Regressor
