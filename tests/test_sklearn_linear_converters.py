084047fda2dd11704cb10ae97b1f9e4825e86978
Karla Saur <kasaur@microsoft.com>
2020-06-15 17:26:31 -0700

    adding comments to test cases

--- tests/test_sklearn_linear_converters.py
+++ tests/test_sklearn_linear_converters.py
@@ -12,6 +12,8 @@ import hummingbird.ml
 
 
 class TestSklearnLinearClassifiers(unittest.TestCase):
+
+    # LogisticRegression test function to be parameterized
     def _test_logistic_regression(self, num_classes, solver="liblinear", multi_class="auto", labels_shift=0):
         if num_classes > 2:
             model = LogisticRegression(solver=solver, multi_class=multi_class, fit_intercept=True)
@@ -30,25 +32,29 @@ class TestSklearnLinearClassifiers(unittest.TestCase):
         self.assertTrue(pytorch_model is not None)
         np.testing.assert_allclose(model.predict_proba(X), pytorch_model.predict_proba(X), rtol=1e-5, atol=1e-6)
 
-    """ TODO ADD COMMENTS ON ALL"""
-
+    # LogisticRegression binary
     def test_logistic_regression_bi(self):
         self._test_logistic_regression(2)
 
+    # LogisticRegression multiclass with auto
     def test_logistic_regression_multi_auto(self):
         self._test_logistic_regression(3)
 
+    # LogisticRegression with class labels shifted
     def test_logistic_regression_shifted_classes(self):
         self._test_logistic_regression(3, labels_shift=2)
 
+    # LogisticRegression with multi+ovr
     def test_logistic_regression_multi_ovr(self):
         self._test_logistic_regression(3, multi_class="ovr")
 
+    # LogisticRegression with multi+multinomial
     def test_logistic_regression_multi_multin(self):
         warnings.filterwarnings("ignore")
         # this will not converge due to small test size
         self._test_logistic_regression(3, multi_class="multinomial", solver="sag")
 
+    # LinearRegression test function to be parameterized
     def _test_linear_regression(self, num_classes):
         model = LinearRegression()
 
@@ -64,12 +70,15 @@ class TestSklearnLinearClassifiers(unittest.TestCase):
         self.assertTrue(pytorch_model is not None)
         np.testing.assert_allclose(model.predict(X), pytorch_model.predict(X), rtol=1e-5, atol=1e-6)
 
+    # LinearRegression with 2 classes
     def test_linear_regression_bi(self):
         self._test_linear_regression(2)
 
+    # LinearRegression with 3 classes
     def test_linear_regression_multi(self):
         self._test_linear_regression(3)
 
+    # LogisticRegressionCV test function to be parameterized
     def _test_logistic_regression_cv(self, num_classes, solver="liblinear", multi_class="auto", labels_shift=0):
         if num_classes > 2:
             model = LogisticRegressionCV(solver=solver, multi_class=multi_class, fit_intercept=True)
@@ -86,23 +95,29 @@ class TestSklearnLinearClassifiers(unittest.TestCase):
         self.assertTrue(pytorch_model is not None)
         np.testing.assert_allclose(model.predict_proba(X), pytorch_model.predict_proba(X), rtol=1e-5, atol=1e-6)
 
+    # LogisticRegressionCV with 2 classes
     def test_logistic_regression_cv_bi(self):
         self._test_logistic_regression_cv(2)
 
+    # LogisticRegressionCV with 3 classes
     def test_logistic_regression_cv_multi(self):
         self._test_logistic_regression_cv(3)
 
+    # LogisticRegressionCV with shifted classes
     def test_logistic_regression_cv_shifted_classes(self):
         self._test_logistic_regression_cv(3, labels_shift=2)
 
+    # LogisticRegressionCV with multi+ovr
     def test_logistic_regression_cv_multi_ovr(self):
         self._test_logistic_regression_cv(3, multi_class="ovr")
 
+    # LogisticRegressionCV with multi+multinomial
     def test_logistic_regression_cv_multi_multin(self):
         warnings.filterwarnings("ignore")
         # this will not converge due to small test size
         self._test_logistic_regression_cv(3, multi_class="multinomial", solver="sag")
 
+    # SGDClassifier test function to be parameterized
     def _test_sgd_classifier(self, num_classes):
 
         model = SGDClassifier(loss="log")
@@ -118,9 +133,11 @@ class TestSklearnLinearClassifiers(unittest.TestCase):
         self.assertTrue(pytorch_model is not None)
         np.testing.assert_allclose(model.predict(X), pytorch_model.predict(X), rtol=1e-5, atol=1e-6)
 
+    # SGDClassifier with 2 classes
     def test_sgd_classifier_bi(self):
         self._test_sgd_classifier(2)
 
+    # SGDClassifier with 3 classes
     def test_sgd_classifier_multi(self):
         self._test_sgd_classifier(3)
 
