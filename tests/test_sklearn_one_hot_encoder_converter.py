f5d12420d51b23e39a4f630b771d525e46ca61b7
Tuan Nguyen <anhtuan277@gmail.com>
2020-06-17 16:29:17 -0700

    try blind conversion

--- tests/test_sklearn_one_hot_encoder_converter.py
+++ tests/test_sklearn_one_hot_encoder_converter.py
@@ -5,9 +5,8 @@ import unittest
 
 import numpy as np
 import torch
-from skl2pytorch import convert_sklearn
-from skl2pytorch.common.data_types import Int64TensorType, Int32TensorType
 from sklearn.preprocessing import OneHotEncoder
+import hummingbird.ml
 
 
 class TestSklearnOneHotEncoderConverter(unittest.TestCase):
@@ -16,7 +15,7 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
         data = np.array([[1, 2, 3], [4, 3, 0], [0, 1, 4], [0, 5, 6]], dtype=np.int64)
         model.fit(data)
 
-        pytorch_model = convert_sklearn(model, [("input", Int64TensorType([4, 3]))])
+        pytorch_model = hummingbird.ml.convert(model, "pytorch")
         self.assertTrue(pytorch_model is not None)
         self.assertTrue(np.allclose(model.transform(data).todense(), pytorch_model(torch.from_numpy(data)).data.numpy()))
 
@@ -28,7 +27,7 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
         # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
         max_word_length = 4
         num_columns = 3
-        pytorch_model = convert_sklearn(model, [("input", Int32TensorType([4, 3, max_word_length // 4]))])
+        pytorch_model = hummingbird.ml.convert(model, "pytorch")
         self.assertTrue(pytorch_model is not None)
 
         pytorch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
