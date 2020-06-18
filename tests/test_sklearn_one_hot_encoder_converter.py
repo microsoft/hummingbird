edcd87e6079b6784c791f1b08b238e459681d525
Tuan Nguyen <anhtuan277@gmail.com>
2020-06-17 17:19:26 -0700

    Update test_sklearn_one_hot_encoder_converter.py

--- tests/test_sklearn_one_hot_encoder_converter.py
+++ tests/test_sklearn_one_hot_encoder_converter.py
@@ -15,7 +15,7 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
         data = np.array([[1, 2, 3], [4, 3, 0], [0, 1, 4], [0, 5, 6]], dtype=np.int64)
         model.fit(data)
 
-        pytorch_model = hummingbird.ml.convert(model, "pytorch", np.array([4,3], dtype=np.int64))
+        pytorch_model = hummingbird.ml.convert(model, "pytorch", np.array([4, 3], dtype=np.int64))
         self.assertTrue(pytorch_model is not None)
         self.assertTrue(np.allclose(model.transform(data).todense(), pytorch_model(torch.from_numpy(data)).data.numpy()))
 
