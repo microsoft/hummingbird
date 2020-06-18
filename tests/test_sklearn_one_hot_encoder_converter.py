b44ed06b874a70084d518d6d71a9629a6961f56e
Tuan Nguyen <anhtuan277@gmail.com>
2020-06-17 17:18:24 -0700

    try adding test_input

--- tests/test_sklearn_one_hot_encoder_converter.py
+++ tests/test_sklearn_one_hot_encoder_converter.py
@@ -15,7 +15,7 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
         data = np.array([[1, 2, 3], [4, 3, 0], [0, 1, 4], [0, 5, 6]], dtype=np.int64)
         model.fit(data)
 
-        pytorch_model = hummingbird.ml.convert(model, "pytorch")
+        pytorch_model = hummingbird.ml.convert(model, "pytorch", np.array([4,3], dtype=np.int64))
         self.assertTrue(pytorch_model is not None)
         self.assertTrue(np.allclose(model.transform(data).todense(), pytorch_model(torch.from_numpy(data)).data.numpy()))
 
@@ -27,7 +27,7 @@ class TestSklearnOneHotEncoderConverter(unittest.TestCase):
         # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
         max_word_length = 4
         num_columns = 3
-        pytorch_model = hummingbird.ml.convert(model, "pytorch")
+        pytorch_model = hummingbird.ml.convert(model, "pytorch", np.array([4, 3, max_word_length // 4], dtype=np.int64))
         self.assertTrue(pytorch_model is not None)
 
         pytorch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
