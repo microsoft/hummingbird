c6187106d6d2f4ab35ad57ad74b2d20f0698ba60
Karla Saur <kasaur@microsoft.com>
2020-07-10 16:33:28 -0700

    stuck at Inference of ONNX models with multiple ONNXML operators is not supported yet.

--- tests/test_onnxml_linear_converter.py
+++ tests/test_onnxml_linear_converter.py
@@ -1,18 +1,16 @@
 """
-Tests onnxmltools->hb conversion for linear models.
+Tests onnxml Normalizer converter
 """
-
-
 import unittest
-import sys
-import os
-import numpy as np
+import warnings
 
+import numpy as np
+import torch
 from sklearn.linear_model import LinearRegression, LogisticRegression, SGDClassifier, LogisticRegressionCV
 from sklearn.svm import LinearSVC, SVC, NuSVC
 
+from hummingbird.ml._utils import onnx_ml_tools_installed, onnx_runtime_installed, lightgbm_installed
 from hummingbird.ml import convert
-from hummingbird.ml._utils import onnx_runtime_installed, onnx_ml_tools_installed
 
 if onnx_runtime_installed():
     import onnxruntime as ort
@@ -20,78 +18,77 @@ if onnx_ml_tools_installed():
     from onnxmltools import convert_sklearn
     from onnxmltools.convert.common.data_types import FloatTensorType as FloatTensorType_onnx
 
-import torch
 
-
-class TestOnnxmlLinearClassifiers(unittest.TestCase):
-    def __init__(self, *args, **kwargs):
-        super(TestOnnxmlLinearClassifiers, self).__init__(*args, **kwargs)
-
+class TestSklearnNormalizer(unittest.TestCase):
     def _test_regressor(self, classes):
         n_features = 20
         n_total = 100
         np.random.seed(0)
+        warnings.filterwarnings("ignore")
         X = np.random.rand(n_total, n_features)
         X = np.array(X, dtype=np.float32)
         y = np.random.randint(classes, size=n_total)
+
+        # Create SKL model for testing
         model = LogisticRegression(solver="liblinear", multi_class="ovr", fit_intercept=True)
         model.fit(X, y)
 
-        # From Sklearn to ONNX-ml to get the input
-        onnx_ml_model = convert_sklearn(model, initial_types=[("float_input", FloatTensorType_onnx([-1, n_features]))])
+        # Create ONNX-ML model
+        onnx_ml_model = convert_sklearn(model, initial_types=[("float_input", FloatTensorType_onnx(X.shape))])
 
-        # Create ONNX model
+        # Create ONNX model by calling converter
         onnx_model = convert(onnx_ml_model, "onnx", X)
-
-        # Now call the new Pytorch model (converted from onnxml) with the data
-        test_run = onnx_model(torch.from_numpy(self.X))[1].data.numpy()
-
-        # compare converted onnx model to pytorch (should be same as above)
-        pytorch_model = convert_sklearn(model)
-        self.assertTrue(np.allclose(pytorch_model(torch.from_numpy(self.X))[1].data.numpy(), test_run))
-
-        #### TODO: Pausing for now until we decide on target output
-
         # Get the predictions for the ONNX-ML model
-        # session = ort.InferenceSession(onnx_ml_model.SerializeToString())
-        # output_names = [session.get_outputs()[i].name for i in range(len(session.get_outputs()))]
-        # onnx_ml_pred = [[] for i in range(len(output_names))]
-        # inputs = {session.get_inputs()[0].name: X}
-        # pred = session.run(output_names, inputs)
-        # for i in range(len(output_names)):
-        #     if output_names[i] == "label":
-        #         onnx_ml_pred[1] = pred[i]
-        #     else:
-        #         onnx_ml_pred[0] = pred[i]
+        session = ort.InferenceSession(onnx_ml_model.SerializeToString())
+        output_names = [session.get_outputs()[i].name for i in range(len(session.get_outputs()))]
+        onnx_ml_pred = [[] for i in range(len(output_names))]
+        inputs = {session.get_inputs()[0].name: X}
+        onnx_ml_pred = session.run(output_names, inputs)
 
-        # # Get the predictions for the ONNX model
-        # session = ort.InferenceSession(onnx_model.SerializeToString())
-        # onnx_pred = [[] for i in range(len(output_names))]
-        # pred = session.run(output_names, inputs)
-        # for i in range(len(output_names)):
-        #     if output_names[i] == "label":
-        #         onnx_pred[1] = pred[i]
-        #     else:
-        #         onnx_pred[0] = pred[i]
+        # Get the predictions for the ONNX model
+        session = ort.InferenceSession(onnx_model.SerializeToString())
+        onnx_pred = [[] for i in range(len(output_names))]
+        onnx_pred = session.run(output_names, inputs)
 
-        # return onnx_ml_pred, onnx_pred, output_names
-        #######
+        return onnx_ml_pred, onnx_pred
 
-    # Check that ONNXML models can only target the ONNX backend.
     @unittest.skipIf(
-        not (onnx_ml_tools_installed() and onnx_runtime_installed()),
-        reason="ONNXML-linear tests require ONNX, ORT and SKL2ONNX",
+        not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
     )
-    def test_logistic_regression_onnxml_binary(self):
-        self._test_regressor(2)
+    def test_logistic_regression_onnxml_binary(self, rtol=1e-06, atol=1e-06):
+        onnx_ml_pred, onnx_pred = self._test_regressor(2)
 
-    # Check that ONNXML models can only target the ONNX backend.
+        # Check that predicted values match
+        np.testing.assert_allclose(onnx_ml_pred, onnx_pred, rtol=rtol, atol=atol)
+
     @unittest.skipIf(
-        not (onnx_ml_tools_installed() and onnx_runtime_installed()),
-        reason="ONNXML-linear tests require ONNX, ORT and SKL2ONNX",
+        not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
     )
-    def test_logistic_regression_onnxml_multi(self):
-        self._test_regressor(3)
+    def test_logistic_regression_onnxml_multi(self, rtol=1e-06, atol=1e-06):
+        onnx_ml_pred, onnx_pred = self._test_regressor(3)
+
+        # Check that predicted values match
+        np.testing.assert_allclose(onnx_ml_pred, onnx_pred, rtol=rtol, atol=atol)
+
+    # @unittest.skipIf(
+    #     not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
+    # )
+    # def test_onnx_linear_model_converter_raises_rt(self):
+    #     np.random.seed(0)
+    #     warnings.filterwarnings("ignore")
+    #     X = np.random.rand(20, 20)
+    #     X = np.array(X, dtype=np.float32)
+    #     # class size 1 should fail
+    #     y = np.random.randint(1, size=20)
+
+    #     # Create SKL model for testing
+    #     model = LogisticRegression(solver="liblinear", multi_class="ovr", fit_intercept=True)
+    #     model.fit(X, y)
+
+    #     # Create ONNX-ML model
+    #     onnx_ml_model = convert_sklearn(model, initial_types=[("float_input", FloatTensorType_onnx(X.shape))])
+
+    #     self.assertRaises(RuntimeError, convert, onnx_ml_model, "onnx", X)
 
 
 if __name__ == "__main__":
