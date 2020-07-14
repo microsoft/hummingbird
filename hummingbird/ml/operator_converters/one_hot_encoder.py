ecdf5cf1df485d12f7522d78b14d2159213ac7e6
Karla Saur <kasaur@microsoft.com>
2020-07-14 11:05:12 -0700

    Scikit learn had API change that removed categorical_features

--- hummingbird/ml/operator_converters/one_hot_encoder.py
+++ hummingbird/ml/operator_converters/one_hot_encoder.py
@@ -32,6 +32,7 @@ class OneHotEncoderString(torch.nn.Module):
         self.condition_tensors = torch.nn.Parameter(torch.IntTensor(condition_tensors), requires_grad=False)
         self.num_categories = num_categories
         self.regression = False
+        self.transformer = True
 
     def forward(self, x):
         encoded_tensors = []
@@ -45,55 +46,27 @@ class OneHotEncoderString(torch.nn.Module):
 
 
 class OneHotEncoder(torch.nn.Module):
-    def __init__(self, indices, retain_order, categories, device):
+    def __init__(self, categories, device):
         super(OneHotEncoder, self).__init__()
 
         self.num_columns = len(categories)
-        self.retain_order = retain_order
-        if indices is None:
-            n_features = self.num_columns
-            self.n_selected = n_features
-        else:
-            n_features = len(indices)
-            ind = np.arange(n_features)
-            sel = np.zeros(n_features, dtype=bool)
-            sel[np.asarray(indices)] = True
-            not_sel = np.logical_not(sel)
-            self.n_selected = np.sum(sel)
-            self.ind_sel = torch.nn.Parameter(torch.LongTensor(ind[sel]), requires_grad=False)
-            self.ind_not_sel = torch.nn.Parameter(torch.LongTensor(ind[not_sel]), requires_grad=False)
-
-        self.all = n_features == self.num_columns
 
         condition_tensors = []
         for arr in categories:
             condition_tensors.append(torch.nn.Parameter(torch.LongTensor(arr), requires_grad=False))
         self.condition_tensors = torch.nn.ParameterList(condition_tensors)
+        self.regression = False
+        self.transformer = True
 
     def forward(self, x):
         if x.dtype != torch.int64:
             x = x.long()
 
-        if self.n_selected == 0:
+        if self.num_columns == 0:
             # No features selected.
             return x
-        elif self.all:
-            # All features selected.
-            return self.transform(x)
-        else:
-            x_sel = self.transform(x[:, self.ind_sel])
 
-        if self.retain_order:
-            pass
-            # This is not yet implemented and future work
-            # (As-s: This does not require impl at this time!!)
-            #
-            ## For the moment it never retain order
-            ## x[:, ind[sel]] = x_sel
-            ## return x
-
-        x_not_sel = x[:, self.ind_not_sel].float()
-        return torch.cat([x_sel, x_not_sel], dim=1)
+        return self.transform(x)
 
     def transform(self, x):
         encoded_tensors = []
@@ -107,7 +80,7 @@ def convert_sklearn_one_hot_encoder(operator, device, extra_config):
         categories = [[str(x) for x in c.tolist()] for c in operator.raw_operator.categories_]
         return OneHotEncoderString(categories, device)
     else:
-        return OneHotEncoder(operator.raw_operator.categorical_features, False, operator.raw_operator.categories_, device)
+        return OneHotEncoder(operator.raw_operator.categories_, device)
 
 
 register_converter("SklearnOneHotEncoder", convert_sklearn_one_hot_encoder)
