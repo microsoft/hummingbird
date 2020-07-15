53e4c5967e6f410d7327a81e6bb8ba6dbfd3cf96
Matteo Interlandi <mainterl@microsoft.com>
2020-07-15 15:52:39 -0700

    Add comments for OHE

--- hummingbird/ml/operator_converters/skl_one_hot_encoder.py
+++ hummingbird/ml/operator_converters/skl_one_hot_encoder.py
@@ -24,29 +24,37 @@ class OneHotEncoderString(BaseOperator, torch.nn.Module):
         self.num_columns = len(categories)
         self.max_word_length = max([max([len(c) for c in cat]) for cat in categories])
 
+        # Strings are casted to int32, therefore we need to properly size the tensor to me dividable by 4.
         while self.max_word_length % 4 != 0:
             self.max_word_length += 1
 
+        # We build condition tensors as a 2d tensor of integers.
+        # The first dimension is of size num words, the second dimension is fixed to the max word length (// 4).
         condition_tensors = []
-        num_categories = [0]
+        categories_idx = [0]
         for arr in categories:
             cats = (
-                np.array(arr, dtype="|S" + str(self.max_word_length))
+                np.array(arr, dtype="|S" + str(self.max_word_length)) # Encode objects into 4 byte strings.
                 .view("int32")
                 .reshape(-1, self.max_word_length // 4)
                 .tolist()
             )
+            # We merge all categories for all columns into a single tensor
             condition_tensors.extend(cats)
-            num_categories.append(num_categories[-1] + len(cats))
+            # Since all categories are merged together, we need to track of indexes to retrieve them at inference time.
+            categories_idx.append(categories_idx[-1] + len(cats))
         self.condition_tensors = torch.nn.Parameter(torch.IntTensor(condition_tensors), requires_grad=False)
-        self.num_categories = num_categories
+        self.categories_idx = categories_idx
 
     def forward(self, x):
         encoded_tensors = []
         for i in range(self.num_columns):
-            conditions = self.condition_tensors[self.num_categories[i] : self.num_categories[i + 1], :].view(
+            # First we fetch the condition for the particular column.
+            conditions = self.condition_tensors[self.categories_idx[i] : self.categories_idx[i + 1], :].view(
                 1, -1, self.max_word_length // 4
             )
+            # Differently than the numeric case where eq is enough, here we need to aggregate per object (dim = 2)
+            # because objects can span multiple integers. We use product here since all ints must match to get encoding of 1.
             encoded_tensors.append(torch.prod(torch.eq(x[:, i : i + 1, :], conditions), dim=2))
 
         return torch.cat(encoded_tensors, dim=1).float()
