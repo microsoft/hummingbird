8a5379ea77424eae7c5159b9e969fb3c3ecda3df
Matteo Interlandi <mainterl@microsoft.com>
2020-08-28 12:27:47 -0700

    add check tvm is installed

--- tests/test_sklearn_scaler_converter.py
+++ tests/test_sklearn_scaler_converter.py
@@ -7,6 +7,7 @@ import torch
 from sklearn.preprocessing import RobustScaler, MaxAbsScaler, MinMaxScaler, StandardScaler
 
 import hummingbird.ml
+from hummingbird.ml._utils import tvm_installed
 
 
 class TestSklearnScalerConverter(unittest.TestCase):
@@ -107,6 +108,8 @@ class TestSklearnScalerConverter(unittest.TestCase):
         self.assertIsNotNone(torch_model)
         np.testing.assert_allclose(model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06)
 
+    # Tests with TVM backend
+    @unittest.skipIf(not tvm_installed(), reason="TVM test requires TVM installed")
     def test_standard_scaler_floats_tvm(self):
         # Generate a random 2D array with values in [0, 1000)
         np.random.seed(0)
