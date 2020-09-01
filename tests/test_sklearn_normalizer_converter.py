f9cdbb2b6f585ec4165a0695e4753c3c656ac5f4
Matteo Interlandi <mainterl@microsoft.com>
2020-09-01 09:36:01 -0700

    add test for onnx and tvm for normalizer

--- tests/test_sklearn_normalizer_converter.py
+++ tests/test_sklearn_normalizer_converter.py
@@ -61,6 +61,44 @@ class TestSklearnNormalizer(unittest.TestCase):
                 model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
             )
 
+    # ONNX backend
+    def test_normalizer_converter_onnx(self):
+        # Generate a random 2D array with values in [0, 1000)
+        np.random.seed(0)
+        data = np.random.rand(100, 200) * 1000
+        data = np.array(data, dtype=np.float32)
+        data_tensor = torch.from_numpy(data)
+
+        for norm in ["l1", "l2", "max"]:
+            model = Normalizer(norm=norm)
+            model.fit(data)
+
+            torch_model = hummingbird.ml.convert(model, "onnx", data)
+
+            self.assertIsNotNone(torch_model)
+            np.testing.assert_allclose(
+                model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
+            )
+
+    # TVM backend
+    def test_normalizer_converter_tvm(self):
+        # Generate a random 2D array with values in [0, 1000)
+        np.random.seed(0)
+        data = np.random.rand(100, 200) * 1000
+        data = np.array(data, dtype=np.float32)
+        data_tensor = torch.from_numpy(data)
+
+        for norm in ["l1", "l2", "max"]:
+            model = Normalizer(norm=norm)
+            model.fit(data)
+
+            torch_model = hummingbird.ml.convert(model, "tvm", data)
+
+            self.assertIsNotNone(torch_model)
+            np.testing.assert_allclose(
+                model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
