76a89ff35c91df2ce43a239f923dceb4eef61870
Matteo Interlandi <mainterl@microsoft.com>
2020-09-01 09:44:58 -0700

    add test for binarizer

--- tests/test_sklearn_binarizer_converter.py
+++ tests/test_sklearn_binarizer_converter.py
@@ -26,6 +26,20 @@ class TestSklearnBinarizer(unittest.TestCase):
                 model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
             )
 
+    def test_binarizer_converter_tvm(self):
+        data = np.array([[1, 2, -3], [4, -3, 0], [0, 1, 4], [0, -5, 6]], dtype=np.float32)
+        data_tensor = torch.from_numpy(data)
+
+        for threshold in [0.0, 1.0, -2.0]:
+            model = Binarizer(threshold=threshold)
+            model.fit(data)
+
+            torch_model = hummingbird.ml.convert(model, "tvm", data)
+            self.assertIsNotNone(torch_model)
+            np.testing.assert_allclose(
+                model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
+            )
+
 
 if __name__ == "__main__":
     unittest.main()
