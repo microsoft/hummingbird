14d890ddaf8ac35099ebe39a8b736d31bdc162f9
Matteo Interlandi <mainterl@microsoft.com>
2020-09-01 09:48:47 -0700

    add test for mlp

--- tests/test_sklearn_mlp_converter.py
+++ tests/test_sklearn_mlp_converter.py
@@ -14,7 +14,7 @@ import hummingbird.ml
 class TestSklearnMLPClassifier(unittest.TestCase):
 
     # MLPClassifier test function to be parameterized
-    def _test_mlp_classifer(self, num_classes, activation="relu", labels_shift=0):
+    def _test_mlp_classifer(self, num_classes, activation="relu", labels_shift=0, backend="torch"):
         model = MLPClassifier(hidden_layer_sizes=(100, 100, 50,), activation=activation)
         np.random.seed(0)
         X = np.random.rand(100, 200)
@@ -22,7 +22,7 @@ class TestSklearnMLPClassifier(unittest.TestCase):
         y = np.random.randint(num_classes, size=100) + labels_shift
 
         model.fit(X, y)
-        torch_model = hummingbird.ml.convert(model, "torch")
+        torch_model = hummingbird.ml.convert(model, "torch", X)
         self.assertTrue(torch_model is not None)
         np.testing.assert_allclose(model.predict_proba(X), torch_model.predict_proba(X), rtol=1e-6, atol=1e-6)
 
@@ -49,6 +49,31 @@ class TestSklearnMLPClassifier(unittest.TestCase):
     #  MLPClassifier multi-class w/ identity activation
     def test_mlp_classifer_multi_identity(self):
         self._test_mlp_classifer(3, activation="identity")
+
+    # Test TVM backend
+    # MLPClassifier binary
+    def test_mlp_classifer_bi_tvm(self):
+        self._test_mlp_classifer(2, backend="tvm")
+
+    # MLPClassifier multi-class
+    def test_mlp_classifer_multi_tvm(self):
+        self._test_mlp_classifer(3, backend="tvm")
+
+    # MLPClassifier multi-class w/ shifted labels
+    def test_mlp_classifer_multi_shifted_labels_tvm(self):
+        self._test_mlp_classifer(3, labels_shift=3, backend="tvm")
+
+    #  MLPClassifier multi-class w/ tanh activation
+    def test_mlp_classifer_multi_logistic_tvm(self):
+        self._test_mlp_classifer(3, activation="tanh", backend="tvm")
+
+    #  MLPClassifier multi-class w/ logistic activation
+    def test_mlp_classifer_multi_tanh_tvm(self):
+        self._test_mlp_classifer(3, activation="logistic", backend="tvm")
+
+    #  MLPClassifier multi-class w/ identity activation
+    def test_mlp_classifer_multi_identity_tvm(self):
+        self._test_mlp_classifer(3, activation="identity", backend="tvm")
 
 
 if __name__ == "__main__":
