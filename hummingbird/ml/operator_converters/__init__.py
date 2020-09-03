eea4a5207ee307476e852b6f720285911bb85c3f
scnakandala <supun.nakandala@gmail.com>
2020-09-03 09:36:19 -0700

    removing tick mark

--- hummingbird/ml/operator_converters/__init__.py
+++ hummingbird/ml/operator_converters/__init__.py
@@ -47,7 +47,7 @@ from . import xgb  # noqa: E402
 __pdoc__ = {}
 __pdoc__["hummingbird.operator_converters._array_feature_extractor_implementations"] = True
 __pdoc__["hummingbird.operator_converters._decomposition_implementations"] = True
-__pdoc__["hummingbird.operator_converters._discretizer_implementations`"] = True
+__pdoc__["hummingbird.operator_converters._discretizer_implementations"] = True
 __pdoc__["hummingbird.operator_converters._gbdt_commons"] = True
 __pdoc__["hummingbird.operator_converters._linear_implementations"] = True
 __pdoc__["hummingbird.operator_converters._mlp_implementations"] = True
