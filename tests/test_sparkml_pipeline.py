21385a46732e54c0ae9e44e048c35b72432a4514
Supun Nakandala <supun.nakandala@gmail.com>
2020-10-04 16:39:00 -0700

    more tests

--- tests/test_sparkml_pipeline.py
+++ tests/test_sparkml_pipeline.py
@@ -86,6 +86,34 @@ class TestSparkMLPipeline(unittest.TestCase):
             torch_model.predict_proba(pd_df), rtol=1e-06, atol=1e-05
         )
 
+    @unittest.skipIf((not sparkml_installed()) or (not pandas_installed()), reason="Spark-ML test requires pyspark and pandas")
+    def test_pipeline3(self):
+        iris = load_iris()
+        features = ['sepal_length', 'sepal_width', 'petal_length', 'petal_width']
+
+        pd_df = pd.DataFrame(data=np.c_[iris['data'], iris['target']], columns=features + ['label'])
+        df = sql.createDataFrame(pd_df)
+
+        quantile1 = QuantileDiscretizer(inputCol='sepal_length', outputCol='sepal_length_bucket', numBuckets=2)
+        quantile2 = QuantileDiscretizer(inputCol='sepal_width', outputCol='sepal_width_bucket', numBuckets=2)
+        features = ['sepal_length_bucket', 'sepal_width_bucket'] + features
+        assembler = VectorAssembler(inputCols=features, outputCol='features')
+        pipeline = Pipeline(stages=[quantile1, quantile2, assembler, LogisticRegression()])
+        model = pipeline.fit(df)
+
+        torch_model = convert(model, "torch", df)
+        self.assertTrue(torch_model is not None)
+
+        np.testing.assert_allclose(
+            np.array(model.transform(df).select("prediction").collect()).reshape(-1),
+            torch_model.predict(pd_df), rtol=1e-06, atol=1e-06
+        )
+
+        np.testing.assert_allclose(
+            np.array(model.transform(df).select("probability").collect()).reshape(-1, 3),
+            torch_model.predict_proba(pd_df), rtol=1e-06, atol=1e-05
+        )
+
 
 if __name__ == "__main__":
     unittest.main()
