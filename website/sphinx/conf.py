0e0d9eb2de88848c4e594abf05ddf4f1770192b4
Karla Saur <kasaur@microsoft.com>
2020-10-23 11:32:01 -0700

    trying to exclude the onnx_operator file

--- website/sphinx/conf.py
+++ website/sphinx/conf.py
@@ -89,7 +89,7 @@ version = "0.1"
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "_templates"]
+exclude_patterns = ["_build", "_templates", "*onnx_operator*"]
 
 # The reST default role (used for this markup: `text`) to use for all
 # documents.
