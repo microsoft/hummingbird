26af73f8203123f185c916fbde8d556c5f116748
Matteo Interlandi <mainterl@microsoft.com>
2020-12-11 12:10:55 -0800

    fix imputation for tvm add tests for tvm and torch.jit

--- tests/test_sklearn_imputer_converter.py
+++ tests/test_sklearn_imputer_converter.py
@@ -19,29 +19,37 @@ import hummingbird.ml
 
 
 class TestSklearnSimpleImputer(unittest.TestCase):
-    def _test_simple_imputer(self, model, data):
+    def _test_simple_imputer(self, model, data, backend):
 
-        data_tensor = torch.from_numpy(data)
         model.fit(data)
 
-        torch_model = hummingbird.ml.convert(model, "torch")
+        hb_model = hummingbird.ml.convert(model, backend, data)
 
-        self.assertIsNotNone(torch_model)
+        self.assertIsNotNone(hb_model)
         np.testing.assert_allclose(
-            model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
+            model.transform(data), hb_model.transform(data), rtol=1e-06, atol=1e-06,
         )
 
     def test_simple_imputer_float_inputs(self):
         model = SimpleImputer(strategy="mean", fill_value="nan")
         data = np.array([[1, 2], [np.nan, 3], [7, 6]], dtype=np.float32)
 
-        self._test_simple_imputer(model, data)
+        for backend in ["torch", "torch.jit", "tvm"]:
+            self._test_simple_imputer(model, data, backend)
 
     def test_simple_imputer_no_nan_inputs(self):
         model = SimpleImputer(missing_values=0, strategy="most_frequent")
         data = np.array([[1, 2], [1, 3], [7, 6]], dtype=np.float32)
 
-        self._test_simple_imputer(model, data)
+        for backend in ["torch", "torch.jit", "tvm"]:
+            self._test_simple_imputer(model, data, backend)
+
+    def test_simple_imputer_nan_to_0(self):
+        model = SimpleImputer(strategy="constant", fill_value=0)
+        data = np.array([[1, 2], [1, 3], [7, 6]], dtype=np.float32)
+
+        for backend in ["torch", "torch.jit", "tvm"]:
+            self._test_simple_imputer(model, data, backend)
 
 
 class TestSklearnImputer(unittest.TestCase):
@@ -73,13 +81,13 @@ class TestSklearnImputer(unittest.TestCase):
 
 
 class TestSklearnMissingIndicator(unittest.TestCase):
-    def _test_sklearn_missing_indic(self, model, data):
+    def _test_sklearn_missing_indic(self, model, data, backend):
         data_tensor = torch.from_numpy(data)
-        torch_model = hummingbird.ml.convert(model, "torch")
-        self.assertIsNotNone(torch_model)
+        hb_model = hummingbird.ml.convert(model, backend, data)
 
+        self.assertIsNotNone(hb_model)
         np.testing.assert_allclose(
-            model.transform(data), torch_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
+            model.transform(data), hb_model.transform(data_tensor), rtol=1e-06, atol=1e-06,
         )
 
     def test_missing_indicator_float_inputs(self):
@@ -87,14 +95,18 @@ class TestSklearnMissingIndicator(unittest.TestCase):
             model = MissingIndicator(features=features)
             data = np.array([[1, 2], [np.nan, 3], [7, 6]], dtype=np.float32)
             model.fit(data)
-            self._test_sklearn_missing_indic(model, data)
+
+            for backend in ["torch", "torch.jit", "tvm"]:
+                self._test_sklearn_missing_indic(model, data, backend)
 
     def test_missing_indicator_float_inputs_isnan_false(self):
         for features in ["all", "missing-only"]:
             model = MissingIndicator(features=features, missing_values=0)
             data = np.array([[1, 2], [0, 3], [7, 6]], dtype=np.float32)
             model.fit(data)
-            self._test_sklearn_missing_indic(model, data)
+
+            for backend in ["torch", "torch.jit", "tvm"]:
+                self._test_sklearn_missing_indic(model, data, backend)
 
 
 if __name__ == "__main__":
