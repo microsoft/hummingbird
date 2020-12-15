3307243d1f4b6b570ff354a3e19220536ad7b3a7
Matteo Interlandi <mainterl@microsoft.com>
2020-12-15 11:33:06 -0800

    fix to label encoder

--- hummingbird/ml/operator_converters/_label_encoder_implementations.py
+++ hummingbird/ml/operator_converters/_label_encoder_implementations.py
@@ -20,24 +20,28 @@ class StringLabelEncoder(BaseOperator, torch.nn.Module):
         super(StringLabelEncoder, self).__init__(transformer=True)
         self.regression = False
         self.num_columns = len(classes)
-        self.max_word_length = max([max([len(c) for c in cat]) for cat in classes])
+        self.max_word_length = max([len(cat) for cat in classes])
         while self.max_word_length % 4 != 0:
             self.max_word_length += 1
 
         data_type = "|S" + str(self.max_word_length)
+        self.max_word_length = self.max_word_length // 4
 
-        # sort the classes and convert to torch.int32
+        # Sort the classes and convert to torch.int32
         classes_conv = torch.from_numpy(np.array(sorted(set(classes)), dtype=data_type).view(np.int32))
+        classes_conv = classes_conv.view(1, -1, self.max_word_length)
 
         self.condition_tensors = torch.nn.Parameter(torch.IntTensor(classes_conv), requires_grad=False)
 
     def forward(self, x):
-        x = x.view(-1, 1)
+        x = x.view(-1, 1, self.max_word_length)
         try:
-            return torch.from_numpy(np.array([(self.condition_tensors == v).nonzero() for v in x]))
-        except ValueError as ve:
+            result = torch.prod(self.condition_tensors == x, dim=2).nonzero(as_tuple=True)[1]
+            assert result.shape[0] == x.shape[0]
+            return result
+        except AssertionError:
             raise ValueError(
-                "{}. x ({}) contains previously unseen labels. condition_tensors: {}".format(ve, x, self.condition_tensors)
+                "x ({}) contains previously unseen labels. condition_tensors: {}".format(x, self.condition_tensors)
             )
 
 
--- tests/test_sklearn_label_encoder_converter.py
+++ tests/test_sklearn_label_encoder_converter.py
@@ -31,13 +31,13 @@ class TestSklearnLabelEncoderConverter(unittest.TestCase):
         model.fit(data)
 
         # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
-        max_word_length = 4
-        num_columns = 4
+        max_word_length = 12
         torch_model = hummingbird.ml.convert(model, "torch")
 
         torch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
-            -1, num_columns, max_word_length // 4
+            -1, max_word_length // 4
         )
+
         np.testing.assert_allclose(model.transform(data), torch_model.transform(torch_input), rtol=1e-06, atol=1e-06)
 
     # if the user gives unseen string input, we should get a ValueError
@@ -52,14 +52,13 @@ class TestSklearnLabelEncoderConverter(unittest.TestCase):
         model.fit(data)
 
         # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
-        max_word_length = 4
-        num_columns = 4
+        max_word_length = 12
         torch_model = hummingbird.ml.convert(model, "torch")
 
         # this isn't in the input data and should give an error.
         data[0] = "milan"
         bad_torch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
-            -1, num_columns, max_word_length // 4
+            -1, max_word_length // 4
         )
 
         self.assertRaises(ValueError, torch_model.transform, bad_torch_input)
