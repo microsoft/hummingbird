7a4f92629f5798e8f01fa67c30862834bf582c52
Matteo Interlandi <mainterl@microsoft.com>
2020-12-15 17:27:11 -0800

    add test for onnxml label encoder on string fix bug in convert when inputs have len(shape) == 1

--- hummingbird/ml/convert.py
+++ hummingbird/ml/convert.py
@@ -181,6 +181,8 @@ def _convert_onnxml(model, backend, test_input, device, extra_config={}):
                     raise RuntimeError("Cannot fetch input shape from ONNX schema. Please provide some test input.")
                 shape = [dim.dim_value for dim in input.type.tensor_type.shape.dim]
 
+                if len(shape) == 1:
+                    shape = [1, shape[0]]
                 assert len(shape) == 2
                 # In ONNX dynamic dimensions will have a shape of 0. Fix the 0-shape in the batch dimension if they exist.
                 if shape[0] == 0:
@@ -196,7 +198,7 @@ def _convert_onnxml(model, backend, test_input, device, extra_config={}):
                     initial_types.append((name, Int64TensorType(shape)))
                 else:
                     raise RuntimeError(
-                        "Input data type {} not supported. Please fill an issue at https://github.com/microsoft/hummingbird/.".format(
+                        "Input data type {} not supported. Please fill an issue at https://github.com/microsoft/hummingbird/, or pass some test_input".format(
                             data_type
                         )
                     )
--- tests/test_onnxml_label_encoder_converter.py
+++ tests/test_onnxml_label_encoder_converter.py
@@ -1,6 +1,7 @@
 """
 Tests onnxml LabelEncoder converter
 """
+from distutils.version import LooseVersion
 import unittest
 import warnings
 
@@ -52,50 +53,47 @@ class TestONNXLabelEncoder(unittest.TestCase):
         np.testing.assert_allclose(onnx_ml_pred, onnx_pred, rtol=1e-06, atol=1e-06)
 
     # Test LabelEncoder with strings
-    # @unittest.skipIf(
-    #     not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
-    # )
-    # def test_model_label_encoder_str_onnxml(self):
-    #     model = LabelEncoder()
-    #     data = [
-    #         "paris",
-    #         "milan",
-    #         "amsterdam",
-    #         "tokyo",
-    #     ]
-    #     model.fit(data)
+    @unittest.skipIf(
+        not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
+    )
+    @unittest.skipIf(
+        LooseVersion(torch.__version__) < LooseVersion("1.8.0"),
+        reason="PyTorch exporter don't support nonzero until version 1.8.0",
+    )
+    def test_model_label_encoder_str_onnxml(self):
+        model = LabelEncoder()
+        data = [
+            "paris",
+            "milan",
+            "amsterdam",
+            "tokyo",
+        ]
+        model.fit(data)
 
-    #     # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
-    #     max_word_length = 12
-    #     view_args = [-1, max_word_length // 4]
-    #     str_dtype = "|S" + str(max_word_length)
-    #     np_arr = np.array(data, dtype=str_dtype).view(np.int32)
+        # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
+        max_word_length = 12
+        view_args = [-1, max_word_length // 4]
+        str_dtype = "|S" + str(max_word_length)
+        np_arr = np.array(data, dtype=str_dtype).view(np.int32)
 
-    #     # pytorch_input = torch.from_numpy(np_arr).view(*view_args)
-    #     data_np = np_arr.reshape(*view_args)
+        # pytorch_input = torch.from_numpy(np_arr).view(*view_args)
+        data_np = np_arr.reshape(*view_args)
 
-    #     onnx_ml_model = convert_sklearn(
-    #         model, initial_types=[("input", LongTensorType_onnx([4, max_word_length // 4]))]
-    #     )
+        onnx_ml_model = convert_sklearn(model, initial_types=[("input", StringTensorType_onnx([4]))])
 
-    #     # TODO: not having the sample input here throws:
-    #     # File "../hummingbird/hummingbird/ml/convert.py", line 184, in _convert_onnxml
-    #     #     assert len(shape) == 2
-    #     #     AssertionError
-    #     # onnx_model = convert(onnx_ml_model, "onnx" )
-    #     onnx_model = convert(onnx_ml_model, "onnx", data_np)
+        onnx_model = convert(onnx_ml_model, "onnx", data_np)
 
-    #     # Get the predictions for the ONNX-ML model
-    #     session = ort.InferenceSession(onnx_ml_model.SerializeToString())
-    #     output_names = [session.get_outputs()[i].name for i in range(len(session.get_outputs()))]
-    #     inputs = {session.get_inputs()[0].name: data}
-    #     onnx_ml_pred = session.run(output_names, inputs)
+        # Get the predictions for the ONNX-ML model
+        session = ort.InferenceSession(onnx_ml_model.SerializeToString())
+        output_names = [session.get_outputs()[i].name for i in range(len(session.get_outputs()))]
+        inputs = {session.get_inputs()[0].name: data}
+        onnx_ml_pred = session.run(output_names, inputs)
 
-    #     # Get the predictions for the ONNX model
-    #     onnx_pred = onnx_model.transform(data_np)
+        # Get the predictions for the ONNX model
+        onnx_pred = onnx_model.transform(data_np)
 
-    #     # Check that predicted values match
-    #     np.testing.assert_allclose(onnx_ml_pred, onnx_pred, rtol=1e-06, atol=1e-06)
+        # Check that predicted values match
+        np.testing.assert_allclose(onnx_ml_pred, onnx_pred, rtol=1e-06, atol=1e-06)
 
     @unittest.skipIf(
         not (onnx_ml_tools_installed() and onnx_runtime_installed()), reason="ONNXML test requires ONNX, ORT and ONNXMLTOOLS"
