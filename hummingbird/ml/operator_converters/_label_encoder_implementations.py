abb07597ee981cb41e7ae800f2b96992d248adfd
Karla Saur <1703543+ksaur@users.noreply.github.com>
2020-12-17 16:01:34 -0800

    onnxml and SKL Label Encoder (#374)
    * [WIP] onnxml Label Encoder
    
    * lint
    
    * adding another case for nfeatures
    
    * fix to label encoder
    
    * fixing nfeatures from rebase
    
    * adding failcase
    
    * checking in progress
    
    * adding test for bad backend intput
    
    * adding skl labelenc
    
    * rebase to #387
    
    * tmp fix so my other tests will also run...
    
    * cleanup
    
    * getting the right error for bad input
    
    * adding failing case for onnxml strings
    
    * fix to label encoder
    
    * fix error
    
    * try with ravel
    
    * adding failcase for onnx strings
    
    * typo
    
    * cleanup
    
    * add test for onnxml label encoder on string
    fix bug in convert when inputs have len(shape) == 1
    
    * add comment on the limit of onnx export for label encoder string
    
    * bumping to torch==1.8 for some cases
    
    * RuntimeError for olderpyt
    
    * removing extra line
    
    * saving 1.8 for another day
    
    Co-authored-by: Matteo Interlandi <mainterl@microsoft.com>

---
+++ hummingbird/ml/operator_converters/_label_encoder_implementations.py
@@ -0,0 +1,59 @@
+# -------------------------------------------------------------------------
+# Copyright (c) 2020 Supun Nakandala. All Rights Reserved.
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
+"""
+Base classes for LabelEncoders
+"""
+
+import torch
+import numpy as np
+
+from ._base_operator import BaseOperator
+
+
+class StringLabelEncoder(BaseOperator, torch.nn.Module):
+    """
+    LabelEncoder over string data types.
+    When the ONNX backend is selected, this operator only works for PyTorch => 1.8.0.
+    """
+
+    def __init__(self, classes, device):
+        super(StringLabelEncoder, self).__init__(transformer=True)
+        self.regression = False
+        self.num_columns = len(classes)
+        self.max_word_length = max([len(cat) for cat in classes])
+        while self.max_word_length % 4 != 0:
+            self.max_word_length += 1
+
+        data_type = "|S" + str(self.max_word_length)
+        self.max_word_length = self.max_word_length // 4
+
+        # Sort the classes and convert to torch.int32
+        classes_conv = torch.from_numpy(np.array(sorted(set(classes)), dtype=data_type).view(np.int32))
+        classes_conv = classes_conv.view(1, -1, self.max_word_length)
+
+        self.condition_tensors = torch.nn.Parameter(torch.IntTensor(classes_conv), requires_grad=False)
+
+    def forward(self, x):
+        x = x.view(-1, 1, self.max_word_length)
+        result = torch.prod(self.condition_tensors == x, dim=2).nonzero(as_tuple=True)[1]
+        assert result.shape[0] == x.shape[0], "x ({}) contains previously unseen labels. condition_tensors: {}".format(
+            x, self.condition_tensors
+        )
+        return result
+
+
+class NumericLabelEncoder(BaseOperator, torch.nn.Module):
+    def __init__(self, classes, device):
+        super(NumericLabelEncoder, self).__init__(transformer=True)
+        self.regression = False
+        self.check_tensor = torch.nn.Parameter(torch.IntTensor(classes), requires_grad=False)
+
+    def forward(self, x):
+        x = x.view(-1, 1)
+
+        return torch.argmax(torch.eq(x, self.check_tensor).int(), dim=1)
---
+++ tests/test_sklearn_label_encoder_converter.py
@@ -0,0 +1,68 @@
+"""
+Tests sklearn LabelEncoder converter
+"""
+import unittest
+
+import numpy as np
+import torch
+from sklearn.preprocessing import LabelEncoder
+import hummingbird.ml
+from onnxconverter_common.data_types import Int32TensorType
+
+
+class TestSklearnLabelEncoderConverter(unittest.TestCase):
+    def test_model_label_encoder(self):
+        model = LabelEncoder()
+        data = np.array([1, 4, 5, 2, 0, 2], dtype=np.int32)
+        model.fit(data)
+
+        torch_model = hummingbird.ml.convert(model, "torch")
+        self.assertTrue(torch_model is not None)
+        np.testing.assert_allclose(model.transform(data), torch_model.transform(data), rtol=1e-06, atol=1e-06)
+
+    def test_model_label_encoder_str(self):
+        model = LabelEncoder()
+        data = [
+            "paris",
+            "tokyo",
+            "amsterdam",
+            "tokyo",
+        ]
+        model.fit(data)
+
+        # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
+        max_word_length = 12
+        torch_model = hummingbird.ml.convert(model, "torch")
+
+        torch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
+            -1, max_word_length // 4
+        )
+
+        np.testing.assert_allclose(model.transform(data), torch_model.transform(torch_input), rtol=1e-06, atol=1e-06)
+
+    # if the user gives unseen string input, we should get a failed assert
+    def test_skl_label_encoder_converter_raises_err(self):
+        model = LabelEncoder()
+        data = [
+            "paris",
+            "tokyo",
+            "amsterdam",
+            "tokyo",
+        ]
+        model.fit(data)
+
+        # max word length is the smallest number which is divisible by 4 and larger than or equal to the length of any word
+        max_word_length = 12
+        torch_model = hummingbird.ml.convert(model, "torch")
+
+        # this isn't in the input data and should give an error.
+        data[0] = "milan"
+        bad_torch_input = torch.from_numpy(np.array(data, dtype="|S" + str(max_word_length)).view(np.int32)).view(
+            -1, max_word_length // 4
+        )
+
+        self.assertRaises(AssertionError, torch_model.transform, bad_torch_input)
+
+
+if __name__ == "__main__":
+    unittest.main()
