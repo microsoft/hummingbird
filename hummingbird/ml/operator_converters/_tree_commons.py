4e240a8b386a446623baa95065550e17d2b5d59a
qin-xiong <77117358+qin-xiong@users.noreply.github.com>
2021-01-12 00:08:50 +0800

    Remove duplicated code (#428)
    The code for getting n_classes is repeated twice. One should be removed.

--- hummingbird/ml/operator_converters/_tree_commons.py
+++ hummingbird/ml/operator_converters/_tree_commons.py
@@ -305,7 +305,6 @@ def get_parameters_for_gemm_common(lefts, rights, features, thresholds, values, 
         features = [0, 0, 0]
         thresholds = [0, 0, 0]
         n_classes = values.shape[1]
-        n_classes = values.shape[1]
         values = np.array([np.zeros(n_classes), values[0], values[0]])
         values.reshape(3, n_classes)
 
