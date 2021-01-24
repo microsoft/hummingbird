4395e4033746e0597453d59cf03c5f01d6931cca
Matteo Interlandi <mainterl@microsoft.com>
2021-01-24 14:35:56 -0800

    fix bug in onnx trees

--- hummingbird/ml/operator_converters/onnx/tree_ensemble.py
+++ hummingbird/ml/operator_converters/onnx/tree_ensemble.py
@@ -116,7 +116,7 @@ def _get_tree_infos_from_onnx_ml_operator(model):
                         l_count += 1
             if t_values.shape[0] == 1:
                 # Model creating trees with just a single leaf node. We fix the values here.
-                n_classes = t_values.shape[1]
+                n_classes = t_values.shape[1] if len(t_values.shape) > 1 else 1
                 t_values = np.array([np.array([0.0]), t_values[0], t_values[0]])
                 t_values.reshape(3, n_classes)
             if is_decision_tree and n_classes == 2:  # We need to fix the probabilities in this case.
@@ -153,7 +153,7 @@ def _get_tree_infos_from_onnx_ml_operator(model):
                 l_count += 1
     if t_values.shape[0] == 1:
         # Model creating trees with just a single leaf node. We fix the values here.
-        n_classes = t_values.shape[1]
+        n_classes = t_values.shape[1] if len(t_values.shape) > 1 else 1
         t_values = np.array([np.array([0.0]), t_values[0], t_values[0]])
         t_values.reshape(3, n_classes)
     if is_decision_tree and n_classes == 2:  # We need to fix the probabilities in this case.
