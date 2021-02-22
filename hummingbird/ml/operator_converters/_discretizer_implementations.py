a537fea9d1a3289371feb57b4549842a694f9036
Supun Nakandala <supun.nakandala@gmail.com>
2021-02-22 13:49:34 -0800

    simplify the discretizer logic

--- hummingbird/ml/operator_converters/_discretizer_implementations.py
+++ hummingbird/ml/operator_converters/_discretizer_implementations.py
@@ -29,22 +29,27 @@ class Binarizer(PhysicalOperator, torch.nn.Module):
 
 
 class KBinsDiscretizer(PhysicalOperator, torch.nn.Module):
-    def __init__(self, logical_operator, encode, bin_edges, labels, device):
+    def __init__(self, logical_operator, encode, n_bins, bin_edges, labels, device):
         super(KBinsDiscretizer, self).__init__(logical_operator)
         self.transformer = True
         self.encode = encode
-        # We use DoubleTensors for better precision.
-        # We use a small delta value of 1e-9.
-        self.ge_tensor = torch.nn.Parameter(torch.FloatTensor(bin_edges[:, :-1]), requires_grad=False)
-        self.lt_tensor = torch.nn.Parameter(torch.FloatTensor(bin_edges[:, 1:]), requires_grad=False)
+
+        self.ge_tensor = torch.FloatTensor(bin_edges[:, 1:-1])
         self.ohe = OneHotEncoder(logical_operator, labels, device)
+        if n_bins is not None:
+            self.n_bins = torch.FloatTensor([[n - 1 for n in n_bins]])
+        else:
+            self.n_bins = None
 
     def forward(self, x):
-        x = x.float()
         x = torch.unsqueeze(x, 2)
-        x = torch.ge(x, self.ge_tensor) & torch.lt(x, self.lt_tensor)
+        x = torch.ge(x, self.ge_tensor)
         x = x.float()
-        x = torch.argmax(x, dim=2, keepdim=False)
+        x = torch.sum(x, dim=2, keepdim=False)
+
+        if self.n_bins is not None:
+            # Clipping the encoded values (Needed for sklearn).
+            x = torch.min(self.n_bins, x)
 
         if self.encode in ["onehot-dense", "onehot"]:
             x = self.ohe(x)
