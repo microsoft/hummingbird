70b1ffd99a85ff64d124029c3424f3d4e0ff9d0c
Matteo Interlandi <mainterl@microsoft.com>
2021-06-08 17:10:17 -0700

    fix atol for regression chain

--- tests/test_sklearn_multioutput_regression.py
+++ tests/test_sklearn_multioutput_regression.py
@@ -57,7 +57,7 @@ class TestSklearnMultioutputRegressor(unittest.TestCase):
 
                 torch_model = hummingbird.ml.convert(model, "torch", extra_config={constants.TREE_THRESHOLD_DTYPE: "float64"})
                 self.assertTrue(torch_model is not None)
-                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-5)
+                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-4)
 
 
 if __name__ == "__main__":
