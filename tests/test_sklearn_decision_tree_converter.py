Fix several numerical precision issues in tree-based models (#511)
* wip

* fixing param order

* fix atol for regression chain

* more fixes

* fixing flake issues

* test data in float32

* test data in float32

* limiting tree depth for perf_tree_trav

* rtol 1e-4

* converting back to float32 after float64 tree operations

* better renaming of variables

* fix order

* wip

* explicitly specify dtype

* removing .predict test. increasing tolerance

* small refactoring

* fix missing self

* just the tensor without dtype is sufficient

* per-init one

Co-authored-by: Matteo Interlandi <mainterl@microsoft.com>
Co-authored-by: snakanda <snakanda@node.testvm.orion-pg0.wisc.cloudlab.us>

tests/test_sklearn_decision_tree_converter.py
@@ -15,6 +15,8 @@ from hummingbird.ml._utils import tvm_installed
 from hummingbird.ml import constants
 from tree_utils import dt_implementation_map
 
+import random
+
 
 class TestSklearnTreeConverter(unittest.TestCase):
     # Check tree implementation
@@ -725,17 +727,25 @@ class TestSklearnTreeConverter(unittest.TestCase):
         for tree_method in ["gemm", "tree_trav", "perf_tree_trav"]:
             for n_targets in [1, 2, 7]:
                 for tree_class in [DecisionTreeRegressor, ExtraTreesRegressor, RandomForestRegressor]:
-                    model = tree_class()
+                    seed = random.randint(0, 2**32 - 1)
+                    if tree_method == "perf_tree_trav":
+                        model = tree_class(random_state=seed, max_depth=10)
+                    else:
+                        model = tree_class(random_state=seed)
                     X, y = datasets.make_regression(
-                        n_samples=100, n_features=10, n_informative=5, n_targets=n_targets, random_state=2021
+                        n_samples=100, n_features=10, n_informative=5, n_targets=n_targets, random_state=seed
                     )
                     model.fit(X, y)
+                    X = X.astype('float32')
+                    y = y.astype('float32')
 
                     torch_model = hummingbird.ml.convert(
-                        model, "torch", extra_config={constants.TREE_IMPLEMENTATION: tree_method}
+                        model,
+                        "torch",
+                        extra_config={constants.TREE_IMPLEMENTATION: tree_method, constants.TREE_OP_PRECISION_DTYPE: "float64"}
                     )
                     self.assertTrue(torch_model is not None)
-                    np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-5)
+                    np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-5, err_msg="{}/{}/{}/{}".format(tree_method, n_targets, tree_class, seed))
 
 
 if __name__ == "__main__":

tests/test_sklearn_multioutput_regression.py
@@ -17,10 +17,9 @@ from sklearn.svm import LinearSVR
 from sklearn.multioutput import MultiOutputRegressor, RegressorChain
 
 import hummingbird.ml
+from hummingbird.ml import constants
 
 import random
-
-random.seed(2021)
 
 
 class TestSklearnMultioutputRegressor(unittest.TestCase):
@@ -28,35 +27,43 @@ class TestSklearnMultioutputRegressor(unittest.TestCase):
     def test_sklearn_multioutput_regressor(self):
         for n_targets in [2, 3, 4]:
             for model_class in [DecisionTreeRegressor, ExtraTreesRegressor, RandomForestRegressor, LinearRegression]:
-                model = MultiOutputRegressor(model_class())
+                seed = random.randint(0, 2**32 - 1)
+                if model_class != LinearRegression:
+                    model = MultiOutputRegressor(model_class(random_state=seed))
+                else:
+                    model = MultiOutputRegressor(model_class())
                 X, y = datasets.make_regression(
-                    n_samples=50, n_features=10, n_informative=5, n_targets=n_targets, random_state=2020
+                    n_samples=50, n_features=10, n_informative=5, n_targets=n_targets, random_state=seed
                 )
                 X = X.astype("float32")
                 y = y.astype("float32")
                 model.fit(X, y)
 
-                torch_model = hummingbird.ml.convert(model, "torch")
+                torch_model = hummingbird.ml.convert(model, "torch", extra_config={constants.TREE_OP_PRECISION_DTYPE: "float64"})
                 self.assertTrue(torch_model is not None)
-                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-4)
+                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-4, err_msg="{}/{}/{}".format(n_targets, model_class, seed))
 
     # Test RegressorChain with different child learners
     def test_sklearn_regressor_chain(self):
         for n_targets in [2, 3, 4]:
             for model_class in [DecisionTreeRegressor, ExtraTreesRegressor, RandomForestRegressor, LinearRegression]:
+                seed = random.randint(0, 2**32 - 1)
                 order = [i for i in range(n_targets)]
-                random.shuffle(order)
-                model = RegressorChain(model_class(), order=order)
+                random.Random(seed).shuffle(order)
+                if model_class != LinearRegression:
+                    model = RegressorChain(model_class(random_state=seed), order=order)
+                else:
+                    model = RegressorChain(model_class(), order=order)
                 X, y = datasets.make_regression(
-                    n_samples=50, n_features=10, n_informative=5, n_targets=n_targets, random_state=2021
+                    n_samples=50, n_features=10, n_informative=5, n_targets=n_targets, random_state=seed
                 )
                 X = X.astype("float32")
                 y = y.astype("float32")
                 model.fit(X, y)
 
-                torch_model = hummingbird.ml.convert(model, "torch")
+                torch_model = hummingbird.ml.convert(model, "torch", extra_config={constants.TREE_OP_PRECISION_DTYPE: "float64"})
                 self.assertTrue(torch_model is not None)
-                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-5, atol=1e-5)
+                np.testing.assert_allclose(model.predict(X), torch_model.predict(X), rtol=1e-4, atol=1e-4, err_msg="{}/{}/{}".format(n_targets, model_class, seed))
 
 
 if __name__ == "__main__":