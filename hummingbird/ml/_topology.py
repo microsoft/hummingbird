225a6f976f6e5202169c116b6712a3693a0859c6
Christopher Hua <chua@squareup.com>
2022-11-25 12:12:23 -0500

    chore: remove `fix_graph`

--- hummingbird/ml/_topology.py
+++ hummingbird/ml/_topology.py
@@ -295,43 +295,6 @@ def convert(topology, backend, test_input, device, extra_config={}):
         if onnx_model_name is not None:
             hb_model.graph.name = onnx_model_name
 
-        # Fix the model to use arbitrary batch dimensions
-        def fix_dim(dim):
-            updated = False
-            if dim.HasField("dim_value"):
-                dim.Clear()
-                updated = True
-                dim.dim_param = "sym"
-
-            return updated
-
-        def fix_value_info(value):
-            num_fixed = 0
-            if value.type.HasField("tensor_type"):
-                shape = value.type.tensor_type.shape
-                if shape:
-                    dim = shape.dim[0]
-                    if fix_dim(dim):
-                        num_fixed += 1
-
-            return num_fixed
-
-        def fix_graph(graph):
-            num_fixed = 0
-            for input in graph.input:
-                num_fixed += fix_value_info(input)
-
-            for output in graph.output:
-                num_fixed += fix_value_info(output)
-
-            for node in graph.node:
-                for attr in node.attribute:
-                    if attr.HasField("g"):
-                        num_fixed += fix_graph(attr.g)
-
-            return num_fixed
-
-        fix_graph(hb_model.graph)
     elif backend == tvm_backend:
         # Pick the proper target.
         if device == "cuda":
