c8a207566d773f07abd9108cd5a16dbaf4943c99
Masahiro Hiramori <mhg00g13@gmail.com>
2024-02-09 20:46:52 +0900

    skip `test_random_forest_tvm_tree_trav_classifier_shifted_labels_converter`

--- tests/test_sklearn_decision_tree_converter.py
+++ tests/test_sklearn_decision_tree_converter.py
@@ -587,6 +587,10 @@ class TestSklearnTreeConverter(unittest.TestCase):
 
     # Random forest tree_trav classifier shifted classes
     @unittest.skipIf(not (tvm_installed()), reason="TVM tests require TVM")
+    @unittest.skipIf(
+        ((sys.platform == "linux") and ("CI" in os.environ) and ("GITHUB_RUN_ID" in os.environ)),
+        reason="This test is flaky on Ubuntu on GitHub Actions. See https://github.com/microsoft/hummingbird/pull/709 for more info.",
+    )
     def test_random_forest_tvm_tree_trav_classifier_shifted_labels_converter(self):
         self._run_tree_classification_converter(
             RandomForestClassifier,
