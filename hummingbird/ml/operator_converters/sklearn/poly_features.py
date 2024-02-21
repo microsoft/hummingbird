3eb83937d3a3776b4b3eb82c1011243833a4bcd3
Pusunuru Giri Prasad <72930035+giriprasad51@users.noreply.github.com>
2024-02-21 23:10:12 +0530

    Polynomial features will support degree from 0 to n (#763)
    * PolynomialFeatures doesn	 support negtive degree
    
    * polyfeatures for n degree
    
    ---------
    
    Co-authored-by: Pusunuru <giri.pusunuru@cgi.com>

--- hummingbird/ml/operator_converters/sklearn/poly_features.py
+++ hummingbird/ml/operator_converters/sklearn/poly_features.py
@@ -10,6 +10,7 @@ Converter for scikit-learn PolynomialFeatures.
 from .._physical_operator import PhysicalOperator
 from onnxconverter_common.registration import register_converter
 import torch
+import itertools
 
 
 class PolynomialFeatures(PhysicalOperator, torch.nn.Module):
@@ -19,40 +20,37 @@ class PolynomialFeatures(PhysicalOperator, torch.nn.Module):
     # TODO extend this class to support higher orders
     """
 
-    def __init__(self, operator, n_features, degree, interaction_only, include_bias, device):
+    def __init__(self, operator, n_features, degree=2, interaction_only=False, include_bias=True, device=None):
         super(PolynomialFeatures, self).__init__(operator)
         self.transformer = True
-
+        self.degree = degree
         self.n_features = n_features
         self.interaction_only = interaction_only
         self.include_bias = include_bias
 
-        indices = [i for j in range(n_features) for i in range(j * n_features + j, (j + 1) * n_features)]
-        self.n_poly_features = len(indices)
-        self.n_features = n_features
-        self.indices = torch.nn.Parameter(torch.LongTensor(indices), requires_grad=False)
-
-        self.bias = torch.nn.Parameter(torch.FloatTensor([1.0]), requires_grad=False)
-
     def forward(self, x):
-        x_orig = x
-        x = x.view(-1, self.n_features, 1) * x.view(-1, 1, self.n_features)
-        x = x.view(-1, self.n_features ** 2)
-        x = torch.index_select(x, 1, self.indices)
+        if self.degree < 0:
+            raise ValueError("Degree should be greater than or equal to 0.")
 
-        # TODO: This gives mismatched elements
-        # if self.interaction_only:
-        #    if self.include_bias:
-        #        bias = self.bias.expand(x_orig.size()[0], 1)
-        #        return torch.cat([bias, x], dim=1)
-        #    else:
-        #        return x
+        features = []
 
+        # Move input to GPU if available
+        device = x.device
+
+        # Add bias term if include_bias is True
         if self.include_bias:
-            bias = self.bias.expand(x_orig.size()[0], 1)
-            return torch.cat([bias, x_orig, x], dim=1)
-        else:
-            return torch.cat([x_orig, x], dim=1)
+            bias = torch.ones(x.size()[0], 1, device=device)
+            features.append(bias)
+
+        # Generate polynomial features
+        for d in range(1, self.degree + 1):
+            for combo in itertools.combinations_with_replacement(range(self.n_features), d):
+                if self.interaction_only and len(set(combo)) != d:
+                    continue
+                new_feature = torch.prod(torch.stack([x[:, idx] for idx in combo], dim=1), dim=1, keepdim=True)
+                features.append(new_feature)
+
+        return torch.cat(features, dim=1).to(device=device)
 
 
 def convert_sklearn_poly_features(operator, device, extra_config):
@@ -71,11 +69,8 @@ def convert_sklearn_poly_features(operator, device, extra_config):
     """
     assert operator is not None, "Cannot convert None operator"
 
-    if operator.raw_operator.interaction_only:
-        raise NotImplementedError("Hummingbird does not currently support interaction_only flag for PolynomialFeatures")
-
-    if operator.raw_operator.degree != 2:
-        raise NotImplementedError("Hummingbird currently only supports degree 2 for PolynomialFeatures")
+    if operator.raw_operator.degree < 0:
+        raise NotImplementedError("Hummingbird does not supports negtive degree for PolynomialFeatures")
     return PolynomialFeatures(
         operator,
         operator.raw_operator.n_features_in_,
