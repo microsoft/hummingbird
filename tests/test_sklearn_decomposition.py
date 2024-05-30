5863b17ac8d20209ae8d2be621d89c4e7c6d8858
kasaur <1703543+ksaur@users.noreply.github.com>
2024-05-30 19:00:55 +0000

    removing todo

--- tests/test_sklearn_decomposition.py
+++ tests/test_sklearn_decomposition.py
@@ -110,7 +110,6 @@ class TestSklearnMatrixDecomposition(unittest.TestCase):
     def test_kernel_pca_converter_precomputed(self):
         self._fit_model_pca(KernelPCA(n_components=5, kernel="precomputed"), precompute=True)
 
-    # TODO: Fails on macos-latest Python 3.8 due to a sklearn bug.
     # FastICA converter with n_components none
     def test_fast_ica_converter_none(self):
         self._fit_model_pca(FastICA(n_components=None))
